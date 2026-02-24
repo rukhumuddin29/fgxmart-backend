4a8e1a87d4d6521ecf8e9fac0deb63c0158eeb3d
rukhumuddinM29 <rukhumuddin@boltgroup.in>
2026-02-24 10:55:56 +0530

    cors updated for frontend and api

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://fgxmart.com', 'https://app.fgxmart.com', 'http://localhost:3000', 'http://localhost:3001'],
+    'allowed_origins' => ['https://fgxmart.com', 'https://api.fgxmart.com', 'https://app.fgxmart.com', 'http://localhost:3000', 'http://localhost:3001'],
 
     'allowed_origins_patterns' => [],
 
